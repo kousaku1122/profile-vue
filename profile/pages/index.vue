742c1b9580d09ad1d59552ad184ec34e90afa9d4
kousaku1122 <kou1122saku@gmail.com>
2021-01-01 14:41:43 +0900

    Feat works skills about

--- profile/pages/index.vue
+++ profile/pages/index.vue
@@ -1,12 +1,20 @@
 <template>
-  <p>hello</p>
+  <div>
+    <About />
+    <Skills />
+    <Works />
+  </div>
 </template>
 
 <script>
-
-
+import About from "@/components/about.vue"
+import Skills from "@/components/skills.vue"
+import Works from "@/components/works.vue"
 export default {
   components: {
+    About,
+    Skills,
+    Works,
   }
 }
 </script>
