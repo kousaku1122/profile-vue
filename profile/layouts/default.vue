006b286d2d53a67293c39f59e2536d5179c48952
kousaku1122 <kou1122saku@gmail.com>
2021-01-01 11:54:47 +0900

    Refactor drawer

--- profile/layouts/default.vue
+++ profile/layouts/default.vue
@@ -5,63 +5,29 @@
         height="400"
         width="100%"
       >
-      <Header @drawer="updateDrawer">
-      </Header>
-        <v-navigation-drawer
-          v-model="drawer"
-          app
-          temporary
-          clipped-right
-          right
-        >
-          <v-list
-            nav
-            dense
-          >
-            <v-list-item-group
-              v-model="group"
-              active-class="deep-purple--text text--accent-4"
-            >
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item>
-
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-account</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Account</v-list-item-title>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-navigation-drawer>
-        <v-card-text>
-        <nuxt />
-    </v-card-text>
-      </v-card>
-
-    <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-main>
-
-
+      <Header @drawer="updateDrawer" />
+      <Drawer v-model="drawer"/>
+      <v-card-text>
+        <v-main>
+          <v-container>
+            <nuxt />
+          </v-container>
+        </v-main>
+      </v-card-text>
+    </v-card>
   </v-app>
 </template>
 
 <script>
 import Header from '@/components/header.vue';
+import Drawer from '@/components/drawer.vue';
 export default {
   data: () => ({
      drawer: false,
-     group: null,
-   }),
+  }),
   components: {
     Header,
+    Drawer,
   },
   methods: {
       updateDrawer (value) {
