695dbc6136ef6368fbceb86eef1430445a461796
kousaku1122 <kou1122saku@gmail.com>
2021-01-04 16:11:08 +0900

    Refactor about code

--- profile/components/about.vue
+++ profile/components/about.vue
@@ -21,86 +21,86 @@
             <v-list-item-title>2018.04</v-list-item-title>
             <v-list-item-subtitle>
               <fa :icon="faCuttlefish" /> c#を講義で学習、プログラミングに興味を持つようになる</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2018.09</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faJava" /> Javaを講義で学習</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.01</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faHtml5" />  <fa :icon="faCss3Alt" /> HTML、CSSを独学で習得</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.04</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faGem" /> Ruby、Ruby on Railsを独学で習得</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.08</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faGem" /> RubyでチームのWebページ作成、Bootstrapを習得</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.09</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faJs" /> Javascriptをインターンで習得</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.10</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faGem" /> Rubyでチームの月会費管理アプリ作成</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.12~</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faVuejs" /> Vueを学習中</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.12~</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faVuejs" /> Joicul参加中</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>2020.01</v-list-item-title>
+            <v-list-item-subtitle>
+              <fa :icon="faVuejs" /> ハッカソン参加予定</v-list-item-subtitle>
             </v-list-item-content>
+
           </v-list-item>
-
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>2018.09</v-list-item-title>
-              <v-list-item-subtitle>
-                <fa :icon="faJava" /> Javaを講義で学習</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-list-item two-line>
-              <v-list-item-content>
-                <v-list-item-title>2020.01</v-list-item-title>
-                <v-list-item-subtitle>
-                  <fa :icon="faHtml5" />  <fa :icon="faCss3Alt" /> HTML、CSSを独学で習得</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-list-item two-line>
-                <v-list-item-content>
-                  <v-list-item-title>2020.04</v-list-item-title>
-                  <v-list-item-subtitle>
-                    <fa :icon="faGem" /> Ruby、Ruby on Railsを独学で習得</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-list-item two-line>
-                  <v-list-item-content>
-                    <v-list-item-title>2020.08</v-list-item-title>
-                    <v-list-item-subtitle>
-                      <fa :icon="faGem" /> RubyでチームのWebページ作成、Bootstrapを習得</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>2020.09</v-list-item-title>
-                      <v-list-item-subtitle>
-                        <fa :icon="faJs" /> Javascriptをインターンで習得</v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-
-                    <v-list-item two-line>
-                      <v-list-item-content>
-                        <v-list-item-title>2020.10</v-list-item-title>
-                        <v-list-item-subtitle>
-                          <fa :icon="faGem" /> Rubyでチームの月会費管理アプリ作成</v-list-item-subtitle>
-                        </v-list-item-content>
-                      </v-list-item>
-
-                      <v-list-item two-line>
-                        <v-list-item-content>
-                          <v-list-item-title>2020.12~</v-list-item-title>
-                          <v-list-item-subtitle>
-                            <fa :icon="faVuejs" /> Vueを学習中</v-list-item-subtitle>
-                          </v-list-item-content>
-                        </v-list-item>
-
-                        <v-list-item two-line>
-                          <v-list-item-content>
-                            <v-list-item-title>2020.12~</v-list-item-title>
-                            <v-list-item-subtitle>
-                              <fa :icon="faVuejs" /> Joicul参加中</v-list-item-subtitle>
-                            </v-list-item-content>
-                          </v-list-item>
-
-                          <v-list-item two-line>
-                            <v-list-item-content>
-                              <v-list-item-title>2020.01</v-list-item-title>
-                              <v-list-item-subtitle>
-                                <fa :icon="faVuejs" /> ハッカソン参加予定</v-list-item-subtitle>
-                              </v-list-item-content>
-
-                            </v-list-item>
-                          </v-card>
-                        </v-col>
-                      </v-row>
-                    </v-container>
-                  </template>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
 <script>
 import { faVuejs } from '@fortawesome/free-brands-svg-icons'
