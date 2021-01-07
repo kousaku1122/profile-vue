aea7c72b26a7e58715e7300905fc4e8facc00646
kousaku1122 <kou1122saku@gmail.com>
2021-01-08 01:07:54 +0900

    Refactor HTML CSS elements

--- profile/components/skills.vue
+++ profile/components/skills.vue
@@ -5,198 +5,26 @@
       <v-divider inset dark></v-divider>
       <div class="text-subtitle-1 text-center py-2">クリックで詳細確認できます</div>
       <v-row justify="space-around">
-        <v-col cols="6" md="3" class="text-center">
-          <v-hover v-slot="{ hover }">
-            <v-card  :elevation="hover ? 12 : 2">
-              <v-dialog
-              width="500"
-              >
-              <template v-slot:activator="{ on, attrs }">
-                <v-img
-                :src="html_image_src"
-                v-bind="attrs"
-                v-on="on"
-                ></v-img>
-              </template>
-              <template v-slot:default="dialog">
-                <v-card>
-                  <v-toolbar
-                  color="primary"
-                  dark
-                  >HTML5</v-toolbar>
-                  <v-card-text>
-                    <v-list-item two-line>
-                      <v-list-item-content>
-                        <v-list-item-title>2019.01</v-list-item-title>
-                        <v-list-item-subtitle class="wrap-text">
-                          独学で学習開始
-                        </v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-
-                    <v-list-item two-line>
-                      <v-list-item-content>
-                        <v-list-item-title>2020.08</v-list-item-title>
-                        <v-list-item-subtitle class="wrap-text">
-                           社会人フットサルチームのWebページ作成
-                         </v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-
-                    <v-list-item two-line>
-                      <v-list-item-content>
-                        <v-list-item-title>2020.09</v-list-item-title>
-                        <v-list-item-subtitle class="wrap-text">
-                          インターンシップでアプリケーション作成</v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-
-                    <v-list-item two-line>
-                      <v-list-item-content>
-                        <v-list-item-title>2020.10</v-list-item-title>
-                        <v-list-item-subtitle class="wrap-text">
-                          社会人フットサルチームの月会費アプリケーション作成
-                          </v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-
-                    <v-list-item two-line>
-                      <v-list-item-content>
-                        <v-list-item-title>2020.12</v-list-item-title>
-                        <v-list-item-subtitle class="wrap-text">
-                          講義で映画レビューページ作成
-                          </v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-
-                    <v-list-item two-line>
-                      <v-list-item-content>
-                        <v-list-item-title></v-list-item-title>
-                        <v-list-item-subtitle class="wrap-text">
-                          その他学習でページ作成
-                          </v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-
-                  </v-card-text>
-                  <v-card-actions class="justify-end">
-                    <v-btn
-                    text
-                    @click="dialog.value = false"
-                    >Close</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </template>
-            </v-dialog>
-            <v-divider></v-divider>
-            <p>HTML5</p>
-          </v-card>
-        </v-hover>
-      </div>
-
-    </v-col>
-      <v-col cols="6" md="3" class="text-center">
-        <v-hover v-slot="{ hover }">
-          <v-card  :elevation="hover ? 12 : 2">
-            <v-dialog
-            width="500"
-            >
-            <template v-slot:activator="{ on, attrs }">
-              <v-img
-              :src="css_image_src"
-              v-bind="attrs"
-              v-on="on"
-              ></v-img>
-            </template>
-            <template v-slot:default="dialog">
-              <v-card>
-                <v-toolbar
-                color="primary"
-                dark
-                >CSS3</v-toolbar>
-                <v-card-text>
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>2019.01</v-list-item-title>
-                      <v-list-item-subtitle class="wrap-text">
-                        独学で学習開始
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>2020.08</v-list-item-title>
-                      <v-list-item-subtitle class="wrap-text">
-                         社会人フットサルチームのWebページ作成,フレームワーク（bootstrap）
-                       </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>2020.09</v-list-item-title>
-                      <v-list-item-subtitle class="wrap-text">
-                        インターンシップでアプリケーション作成</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>2020.10</v-list-item-title>
-                      <v-list-item-subtitle class="wrap-text">
-                        社会人フットサルチームの月会費アプリケーション作成,フレームワーク（bootstrap）
-                        </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>2020.12</v-list-item-title>
-                      <v-list-item-subtitle class="wrap-text">
-                        講義で映画レビューページ作成
-                        </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title></v-list-item-title>
-                      <v-list-item-subtitle class="wrap-text">
-                        その他学習でページ作成
-                        </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-
-                </v-card-text>
-                <v-card-actions class="justify-end">
-                  <v-btn
-                  text
-                  @click="dialog.value = false"
-                  >Close</v-btn>
-                </v-card-actions>
-              </v-card>
-            </template>
-          </v-dialog>
-          <v-divider></v-divider>
-          <p>CSS3</p>
-        </v-card>
-      </v-hover>
-    </div>
-  </v-col>
-  </v-row>
-</v-card>
-</v-container>
+        <Html />
+        <Css />
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import Html from '@/components/html.vue';
+import Css from '@/components/css.vue';
+
 export default {
   data () {
     return {
       dialog: false,
-      html_image_src: require("@/assets/image/html.png"),
-      css_image_src: require("@/assets/image/css.png"),
     }
+  },
+  components: {
+    Html,
+    Css,
   },
 }
 </script>
