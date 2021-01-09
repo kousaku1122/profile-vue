155a89035d2e9f93f5fb5027311991eb64036e04
kousaku1122 <kou1122saku@gmail.com>
2021-01-09 18:10:08 +0900

    Docs title rename

--- profile/nuxt.config.js
+++ profile/nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - profile',
-    title: 'profile',
+    titleTemplate: '%s',
+    title: 'Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
