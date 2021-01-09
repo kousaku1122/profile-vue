ecdda04b4360bf49c1bf2e2700555476e6868c64
kousaku1122 <kou1122saku@gmail.com>
2021-01-09 17:55:53 +0900

    Feat works

---
+++ profile/components/aueluaccounting.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-img
+  :src="image_src"
+  v-bind="attrs"
+  v-on="on"
+  ></v-img>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        image_src: require("@/assets/image/aueluaccounting.png"),
+      }
+  }
+}
+</script>
--- profile/components/works.vue
+++ profile/components/works.vue
@@ -1,5 +1,34 @@
 <template>
-  <div >
-    
-  </div>
+  <v-container fluid>
+    <v-card>
+      <v-tabs v-model="tab">
+        <v-tab href="#tab-1"> AueluWeb </v-tab>
+        <v-tab href="#tab-2"> AueluAccounting</v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item value="tab-1" >
+          <AueluWeb />
+        </v-tab-item>
+        <v-tab-item value="tab-2">
+          <AueluAccounting />
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-container>
 </template>
+
+<script>
+import AueluWeb from '@/components/aueluweb.vue';
+import AueluAccounting from '@/components/aueluaccounting.vue';
+export default {
+  data() {
+    return {
+      tab: 'tab-1',
+    }
+  },
+  components: {
+    AueluWeb,
+    AueluAccounting,
+  },
+}
+</script>
