bdf7ee4f41c26e8054cfced4c9a4a0e88adb85fa
kousaku1122 <kou1122saku@gmail.com>
2021-01-12 11:33:58 +0900

    Refactor &copy to ©

--- profile/components/footer.vue
+++ profile/components/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer>
     <v-spacer></v-spacer>
-    <div>&copy; {{ new Date().getFullYear() }} Koki Sakurai</div>
+    <div>© {{ new Date().getFullYear() }} Koki Sakurai</div>
   </v-footer>
 </template>
