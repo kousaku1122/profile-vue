6d79550819a87c5faf788233c6341ba32ce6049f
kousaku1122 <kou1122saku@gmail.com>
2021-01-15 00:21:30 +0900

    Refactor tabs

---
+++ components/work.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-tabs-items :value="value" @change="$emit('change', value)">
+    <div v-for="tab in tabs" :key="tab.value">
+      <v-tab-item :value="`${tab.value}`" eager>
+        <v-card flat>
+          <v-btn
+            rounded
+            :href="`${tab.url}`"
+            min-width="100%"
+            style="text-transform: none"
+            class="mt-3 mb-2 text-caption"
+          >
+            <v-icon>{{ icon }}</v-icon>
+            {{ tab.url }}
+          </v-btn>
+          <v-container
+            id="scroll-target"
+            style="max-height: 500px"
+            class="overflow-y-auto"
+          >
+            <v-container>
+              <v-img :src="`${tab.img}`" eager></v-img>
+            </v-container>
+          </v-container>
+
+          <div class="text-body-2 pl-8 py-5" v-html="`${tab.sentence}`"></div>
+        </v-card>
+      </v-tab-item>
+    </div>
+  </v-tabs-items>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {
+      icon: "mdi-lock",
+      tabs: [
+        {
+          value: "tab-1",
+          url: "https://www.teamauelu.me/",
+          img: require("@/assets/image/aueluweb.png"),
+          sentence:
+            "大阪府社会人フットサルチームのWebページになります．<br />大阪府社会人フットサルというキーワードでGoogle検索8位でヒット．友人との共同開発作品．<br />機能：選手，試合結果，練習予定表示機能，変更時のニュース追加機能<br />開発言語：HTML、CSS、Ruby、Ruby on Rails、Javascript <br />使用技術：Amazon S3"
+        },
+        {
+          value: "tab-2",
+          url: "https://aueluaccounting.herokuapp.com/",
+          img: require("@/assets/image/aueluaccounting.png"),
+          sentence:
+            "大阪府社会人フットサルチームの月会費管理アプリになります．<br />会計で直面した問題を解決すべく作成したアプリケーションです．<br />機能：背番号検索機能，支払い遅延色変更機能<br />開発言語：HTML、CSS、Ruby、Ruby on Rails、Javascript <br />使用技術：Rspec"
+        }
+      ]
+    };
+  }
+};
+</script>
--- components/works.vue
+++ components/works.vue
@@ -6,69 +6,17 @@
       <div class="text-subtitle-1 text-center py-2">
         リンクをクリックすることでページに飛びます
       </div>
-      <v-tabs v-model="tab" show-arrows fixed-tabs>
+      <v-tabs :value="tab" @change="tab = $event" show-arrows fixed-tabs>
         <v-tab href="#tab-1" class="text-caption"> AueluWeb </v-tab>
         <v-tab href="#tab-2" class="text-caption"> AueluAccounting</v-tab>
       </v-tabs>
-      <v-tabs-items v-model="tab">
-        <v-tab-item value="tab-1" eager>
-          <v-card flat>
-            <v-btn
-              rounded
-              href="https://www.teamauelu.me/"
-              min-width="100%"
-              style="text-transform: none"
-              class="mt-3 mb-2 text-caption"
-            >
-              <v-icon>mdi-lock</v-icon>
-              https://www.teamauelu.me/
-            </v-btn>
-            <v-container
-              id="scroll-target"
-              style="max-height: 500px"
-              class="overflow-y-auto"
-            >
-              <AueluWeb />
-            </v-container>
-            <div class="text-body-2 pl-8 py-5">
-              大阪府社会人フットサルチームのWebページになります．<br />大阪府社会人フットサルというキーワードでGoogle検索8位でヒット．友人との共同開発作品．<br />機能：選手，試合結果，練習予定表示機能，変更時のニュース追加機能<br />開発言語：HTML、CSS、Ruby、Ruby
-              on Rails、Javascript <br />使用技術：Amazon S3
-            </div>
-          </v-card>
-        </v-tab-item>
-        <v-tab-item value="tab-2" eager>
-          <v-card flat>
-            <v-btn
-              rounded
-              href="https://aueluaccounting.herokuapp.com/"
-              min-width="100%"
-              style="text-transform: none"
-              class="mt-3 mb-2 text-caption"
-            >
-              <v-icon>mdi-lock</v-icon>
-              https://aueluaccounting.herokuapp.com/
-            </v-btn>
-            <v-container
-              id="scroll-target"
-              style="max-height: 500px"
-              class="overflow-y-auto"
-            >
-              <AueluAccounting />
-            </v-container>
-            <div class="text-body-2 pl-8 py-5">
-              大阪府社会人フットサルチームの月会費管理アプリになります．<br />会計で直面した問題を解決すべく作成したアプリケーションです．<br />機能：背番号検索機能，支払い遅延色変更機能<br />開発言語：HTML、CSS、Ruby、Ruby
-              on Rails、Javascript <br />使用技術：Rspec
-            </div>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+      <Work v-model="tab" />
     </v-card>
   </v-container>
 </template>
 
 <script>
-import AueluWeb from "@/components/aueluweb.vue";
-import AueluAccounting from "@/components/aueluaccounting.vue";
+import Work from "@/components/work.vue";
 export default {
   data() {
     return {
@@ -76,8 +24,7 @@ export default {
     };
   },
   components: {
-    AueluWeb,
-    AueluAccounting
+    Work
   }
 };
 </script>
