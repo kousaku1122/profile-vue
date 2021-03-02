41d2384f119830b3fdaf8efdbdf146f6be8bcba5
kousaku1122 <kou1122saku@gmail.com>
2021-03-02 12:52:40 +0900

    Add work

--- components/work/WorkList.vue
+++ components/work/WorkList.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid id="works">
     <v-card>
-      <div class="title pb-2 text-center" v-text="Works" />
+      <div class="title pb-2 text-center" v-text="title" />
 
       <v-divider dark></v-divider>
 
@@ -9,6 +9,8 @@
       <v-tabs :value="tab" @change="tab = $event" show-arrows fixed-tabs>
         <v-tab href="#tab-1" class="text-caption" v-text="workTitle.work1" />
         <v-tab href="#tab-2" class="text-caption" v-text="workTitle.work2" />
+        <v-tab href="#tab-3" class="text-caption" v-text="workTitle.work3" />
+        <v-tab href="#tab-4" class="text-caption" v-text="workTitle.work4" />
       </v-tabs>
       <work-list-item v-model="tab" />
     </v-card>
@@ -21,7 +23,12 @@ export default {
   data() {
     return {
       title: "Works",
-      workTitle: { work1: "AueluWeb", work2: "Aueluaccounting" },
+      workTitle: {
+        work1: "AueluWeb",
+        work2: "Aueluaccounting",
+        work3: "Tweetlist",
+        work4: "ExaIntern"
+      },
       description: "リンクをクリックすることでページに飛びます",
       tab: "tab-1"
     };
--- components/work/WorkListItem.vue
+++ components/work/WorkListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <v-tabs-items :value="value" @change="$emit('change', value)">
-    <div v-for="tab in tabs" :key="tab.value">
+    <div v-for="(tab, i) in tabs" :key="i">
       <v-tab-item :value="`${tab.value}`" eager>
         <v-card flat>
           <v-btn
@@ -10,9 +10,10 @@
             style="text-transform: none"
             class="mt-3 mb-2 text-caption"
           >
-            <v-icon>{{ icon }}</v-icon>
+            <v-icon v-text="icon" />
             {{ tab.url }}
           </v-btn>
+
           <v-container
             id="scroll-target"
             style="max-height: 500px"
@@ -42,14 +43,28 @@ export default {
           url: "https://www.teamauelu.me/",
           img: require("@/assets/image/aueluweb.png"),
           sentence:
-            "大阪府社会人フットサルチームのWebページになります．<br />大阪府社会人フットサルというキーワードでGoogle検索8位でヒット．友人との共同開発作品．<br />機能：選手，試合結果，練習予定表示機能，変更時のニュース追加機能<br />開発言語：HTML、CSS、Ruby、Ruby on Rails、Javascript <br />使用技術：Amazon S3"
+            "大阪府社会人フットサルチームのWebページになります．<br />大阪府社会人フットサルというキーワードでGoogle検索6位にヒット．友人との共同開発作品．<br />機能：選手、試合結果、練習予定表示機能、変更時のニュース追加機能、インスタグラム連動<br />開発言語：HTML、CSS、Ruby、Ruby on Rails、Javascript、Vue(Vuetify) <br />使用技術：Amazon S3、InstagramAPI"
         },
         {
           value: "tab-2",
           url: "https://aueluaccounting.herokuapp.com/",
           img: require("@/assets/image/aueluaccounting.png"),
           sentence:
-            "大阪府社会人フットサルチームの月会費管理アプリになります．<br />会計で直面した問題を解決すべく作成したアプリケーションです．<br />機能：背番号検索機能，支払い遅延色変更機能<br />開発言語：HTML、CSS、Ruby、Ruby on Rails、Javascript <br />使用技術：Rspec"
+            "個人情報が掲載されているため、BASIC認証を導入しています。</br>現在は会計引継ぎに伴い使用はしていません。</br>大阪府社会人フットサルチームの月会費管理アプリになります．<br />会計で直面した問題を解決すべく作成したアプリケーションです．<br />機能：背番号検索機能，支払い遅延色変更機能<br />開発言語：HTML、CSS、Ruby、Ruby on Rails、Javascript <br />使用技術：Rspec <br /> コードに関してはこちらからご確認いただけます。<a>https://github.com/kousaku1122/auelu_accounting</a>"
+        },
+        {
+          value: "tab-3",
+          url: "https://github.com/sofuetakuma112/tweet_list/tree/app/",
+          img: require("@/assets/image/hack.png"),
+          sentence:
+            "2daysハッカソンの成果物になります。<br />デプロイしていないため、コードのみになります。<br />ツイートの保存リストを共有できるアプリケーション。<br />担当：フロントエンド<br />機能：ツイート検索、保存リスト作成、保存リスト共有<br />開発言語：HTML、CSS、Vue(Vuetify) 、node.js<br />使用技術：Firebase、TweetAPI"
+        },
+        {
+          value: "tab-4",
+          url: "https://exa-intern.glitch.me/",
+          img: require("@/assets/image/exa-intern.png"),
+          sentence:
+            "5daysインターンでの成果物です。<br />機能：感想投稿機能、いいね機能<br />開発言語：HTML、CSS、Javascript <br />使用技術：Glitch"
         }
       ]
     };
